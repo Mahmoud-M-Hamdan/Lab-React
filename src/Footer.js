d384ec51565c80a9882c2ccbd67fc5d871837e81
Mahmoud-M-Hamdan <eng.mech.hamdan@gmail.com>
2021-09-05 17:37:30 +0300

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+
+
+import { React, Component } from 'react'
+
+class Footer extends Component {
+   render() {
+       return (
+           <div>
+               <p>all righ reserved &copy; Mahmoud Hamdan</p>
+           </div>
+       )
+   }
+}
+
+export default Footer
